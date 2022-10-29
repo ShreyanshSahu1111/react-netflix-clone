9c4398b84ff2910425a6154b05e9d526e21508b7
ShreyanshSahu1111 <87320891+ShreyanshSahu1111@users.noreply.github.com>
2022-10-29 16:40:43 +0530

    Banner done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -24,8 +24,32 @@ const Main = () => {
   }, [])
   // ============================================================================
 
+  const truncateString = (str, limit) => {
+    if(str?.length>limit){
+      return str.slice(0,limit) + '...';
+    }
+    else {
+      return str;
+    }
+  };
+
   return (
-    <div>
+    <div className='w-full h-[550px] text-white'>
+      <div className='w-full h-full'>
+        <div className='absolute w-full h-full bg-gradient-to-r from-black'/>
+        <img className='w-full h-full' src={movie?.backdrop_path} alt={movie?.original_title+" backdrop"} />
+        <div className='absolute w-full top-40 p-4 md:p-8'>
+          <h1 className='text-3xl md:text-5xl font-bold '>{movie?.original_title}</h1>
+          <div className='my-4'>
+            <button className='border bg-gray-300 text-black border-gray-300 rounded py-2 px-5'>Play</button>
+            <button className='border text-white border-gray-300 rounded py-2 px-5 ml-4'>Watch Later</button>
+          </div>
+          <p className='text-gray-400 text-sm'>Released: {movie?.release_date}</p>
+          <p className='w-full md:w-[50%] ld:w-[20%] text-gray-300'>
+            {truncateString(movie?.overview, 150)}
+          </p>
+        </div>
+      </div>
       
     </div>
   )
